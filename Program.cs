19b77495721ff8b584871465f67bb6438cdff7d5
Alyona Rogova@git config --global user.email rogova.photo@mail.rugit config --global user.name Alyona <rogova.photo@mail.ru>
2022-09-30 14:42:47 +0500

    All done

--- Program.cs
+++ Program.cs
@@ -44,6 +44,36 @@ namespace Seminar9HomeWork
            
 
             //Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+            // int ReadData(string line)
+            // {
+            //     Console.Write(line);
+            //     int num = int.Parse(Console.ReadLine() ?? " ");
+            //     return num;
+            // }
+
+            // //Рекурсия суммы чисел от M до N
+            // int SumMN(int M, int N)
+            // {
+            //     if (M >= N) return N;
+            //     return M + SumMN(M+1,N);
+            // }
+
+            // //Вывод результата
+            // void PrintData(string prefix, int num)
+            // {
+            // Console.WriteLine(prefix +  num);
+            // }
+
+
+            // int M = ReadData("Введите число M:");
+            // int N = ReadData("Введите число N:");
+            // int sum  = (M<N)
+            // ?(SumMN(M,N))
+            // :(SumMN(N,M));
+            // PrintData("Сумма чисел от M до N: ", sum);
+           
+            //Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа M и N.
+
             int ReadData(string line)
             {
                 Console.Write(line);
@@ -51,13 +81,22 @@ namespace Seminar9HomeWork
                 return num;
             }
 
-            //Рекурсия суммы чисел от M до N
-            int SumMN(int M, int N)
+            //Рекурсия функции
+            int Akkerman(int M, int N)
             {
-                if (M >= N) return N;
-                return M + SumMN(M+1,N);
+                if (M == 0)
+                {
+                    return N + 1;
+                }
+                else if (N == 0 && M > 0)
+                {
+                    return Akkerman(M - 1, 1);
+                }
+                else
+                {
+                    return (Akkerman(M - 1, Akkerman(M, N - 1)));
+                }
             }
-
             //Вывод результата
             void PrintData(string prefix, int num)
             {
@@ -67,13 +106,9 @@ namespace Seminar9HomeWork
 
             int M = ReadData("Введите число M:");
             int N = ReadData("Введите число N:");
-            int sum  = (M<N)
-            ?(SumMN(M,N))
-            :(SumMN(N,M));
-            PrintData("Сумма чисел от M до N: ", sum);
+            int function = Akkerman (M,N); 
+            PrintData("Функция Аккермана равна: ", function);
            
-            //Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
-
 
         }
     }
